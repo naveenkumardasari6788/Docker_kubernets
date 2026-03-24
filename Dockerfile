45aa2436a3ed96b8a26acc3074e7aa75645a7d6d
naveenkumar6788 <naveenkumardasari6788@gmail.com>
2026-03-24 15:49:37 +0530

    commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM eclipse-temurin:17-jdk-alpine
+WORKDIR /app
+
+COPY target/*.jar cse.jar
+
+CMD ["java","-jar","cse.jar"]
---
+++ src/main/java/com/example/demo/CSECONTROLLER.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CSECONTROLLER {
+	@GetMapping("/cse/home")
+	public String rocks() {
+		return "WELCOME TO CSE KUBERNETES";
+	}
+}
