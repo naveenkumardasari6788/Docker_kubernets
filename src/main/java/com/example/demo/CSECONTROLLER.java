7ad2cb928106cbd7ba4601269a38aa84da567700
naveenkumar6788 <naveenkumardasari6788@gmail.com>
2026-03-28 15:54:24 +0530

    aaa

--- src/main/java/com/example/demo/CSECONTROLLER.java
+++ src/main/java/com/example/demo/CSECONTROLLER.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CSECONTROLLER {
 	@GetMapping("/cse/home")
 	public String rocks() {
-		return "WELCOME TO CSE KUBERNETES";
+		return "WELCOME TO OPENSHIFT";
 	}
 }
 	
